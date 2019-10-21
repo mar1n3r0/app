322a0fa8b9640bb865b666e772577abd3b852b0d
Maxence Charriere <charriere@outlook.com>
2019-10-21 14:22:09 +0200

    Update js.go

--- pkg/app/js.go
+++ pkg/app/js.go
@@ -16,23 +16,80 @@ const (
 )
 
 // Value is the interface that represents a JavaScript value. On wasm
-// architecture, it wraps the https://golang.org/pkg/syscall/js/ package.
+// architecture, it wraps the Value from https://golang.org/pkg/syscall/js/
+// package.
 type Value interface {
+	// Bool returns the value v as a bool. It panics if v is not a JavaScript
+	// boolean.
 	Bool() bool
+
+	// Call does a JavaScript call to the method m of value v with the given
+	// arguments. It panics if v has no method m. The arguments get mapped to
+	// JavaScript values according to the ValueOf function.
 	Call(m string, args ...interface{}) Value
+
+	// Float returns the value v as a float64. It panics if v is not a
+	// JavaScript number.
 	Float() float64
+
+	// Get returns the JavaScript property p of value v. It panics if v is not a
+	// JavaScript object.
 	Get(p string) Value
+
+	// Index returns JavaScript index i of value v. It panics if v is not a
+	// JavaScript object.
 	Index(i int) Value
+
+	// InstanceOf reports whether v is an instance of type t according to
+	// JavaScript's instanceof operator.
 	InstanceOf(t Value) bool
+
+	// Int returns the value v truncated to an int. It panics if v is not a
+	// JavaScript number.
 	Int() int
+
+	// Invoke does a JavaScript call of the value v with the given arguments. It
+	// panics if v is not a JavaScript function. The arguments get mapped to
+	// JavaScript values according to the ValueOf function.
 	Invoke(args ...interface{}) Value
+
+	// JSValue implements Wrapper interface.
 	JSValue() Value
+
+	// Length returns the JavaScript property "length" of v. It panics if v is
+	// not a JavaScript object.
 	Length() int
+
+	// New uses JavaScript's "new" operator with value v as constructor and the
+	// given arguments. It panics if v is not a JavaScript function. The
+	// arguments get mapped to JavaScript values according to the ValueOf
+	// function.
 	New(args ...interface{}) Value
+
+	// Set sets the JavaScript property p of value v to ValueOf(x). It panics if
+	// v is not a JavaScript object.
 	Set(p string, x interface{})
+
+	// SetIndex sets the JavaScript index i of value v to ValueOf(x). It panics
+	// if v is not a JavaScript object.
 	SetIndex(i int, x interface{})
+
+	// String returns the value v as a string. String is a special case because
+	// of Go's String method convention. Unlike the other getters, it does not
+	// panic if v's Type is not TypeString. Instead, it returns a string of the
+	// form "<T>" or "<T: V>" where T is v's type and V is a string
+	// representation of v's value.
 	String() string
+
+	// Truthy returns the JavaScript "truthiness" of the value v. In JavaScript,
+	// false, 0, "", null, undefined, and NaN are "falsy", and everything else
+	// is "truthy". See
+	// https://developer.mozilla.org/en-US/docs/Glossary/Truthy.
 	Truthy() bool
+
+	// Type returns the JavaScript type of the value v. It is similar to
+	// JavaScript's typeof operator, except that it returns TypeNull instead of
+	// TypeObject for null.
 	Type() Type
 }
 
@@ -105,4 +162,10 @@ type BrowserWindow interface {
 // Window returns the JavaScript "window" object.
 func Window() BrowserWindow {
 	panic("not implemented")
+}
+
+// Wrapper is implemented by types that are backed by a JavaScript value.
+type Wrapper interface {
+	// JSValue returns a JavaScript value associated with an object.
+	JSValue() Value
 }
